acc3391f6f960caab2d1cf42716badcb90c89569
LucaFabbozzo <lucafabbozzo@gmail.com>
2025-05-18 17:36:04 +0200

    Implement basic calculator functionality and add .gitignore file

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Hello!");
+
+Console.Write("Input the first number: ");
+var userInputOne = Console.ReadLine();
+int numberOne = int.Parse(userInputOne);
+
+Console.Write("Input the second number: ");
+var userInputTwo = Console.ReadLine();
+int numberTwo = int.Parse(userInputTwo);
+
+Console.WriteLine("What do you want to do with those numbers?");
+Console.WriteLine("[A]dd");
+Console.WriteLine("[S]ubtract");
+Console.WriteLine("[M]ultiply");
+
+var userInput = Console.ReadLine().ToUpper();
+if (userInput == "A")
+{
+    Console.WriteLine((numberOne + numberTwo));
+}
+else if (userInput == "S")
+{
+    Console.WriteLine((numberOne - numberTwo));
+}
+else if (userInput == "M")
+{
+    Console.WriteLine((numberOne * numberTwo));
+}
+
+Console.Write("Press any key to close ");
+var closeInput = Console.ReadKey();
--- Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net9.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c2f61cd86f0596fcb45f822e9c44e81c9ceef2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
